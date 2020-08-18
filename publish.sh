90ee115fc5f5479cb1078e837db62331211cf2b1
xionghao <xionghao930316@gmail.com>
2020-08-18 21:16:56 +0800

    版本测试

--- publish.sh
+++ publish.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+export https_proxy=127.0.0.1:1087
+export http_proxy=127.0.0.1:1087
+
 flutter packages pub publish --dry-run
 flutter packages pub publish --server=https://pub.dartlang.org
 #flutter pub publish -v
